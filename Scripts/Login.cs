a483ce9e66fdff4fae0fa27c9780b07a5eb69c0b
adansif <adan.yanesgonzalez@gmail.com>
2024-05-20 17:22:53 +0100

    feat: Added final logic to Login

--- Scripts/Login.cs
+++ Scripts/Login.cs
@@ -6,14 +6,19 @@ public partial class Login : Control
 	private HttpRequest request;
 	private LineEdit username, password;
 	private Button loginButton;
-
-	private String apiUrl = "http://localhost:8082/api/v1/users/login";
+	private String apiUrl = "http://localhost:8082/api/v1/players/login";
+	private Timer timer;
+	private Label errorLabel;
+	private AnimationPlayer animation;
 	public override void _Ready()
 	{
 		username = this.GetNode<Panel>("./Panel").GetNode<VBoxContainer>("./VBoxContainer").GetNode<LineEdit>("./Username");
 		password = this.GetNode<Panel>("./Panel").GetNode<VBoxContainer>("./VBoxContainer").GetNode<LineEdit>("./Password");
-
 		loginButton = this.GetNode<Panel>("./Panel").GetNode<VBoxContainer>("./VBoxContainer").GetNode<Button>("./LoginButton");
+
+		animation = this.GetNode<AnimationPlayer>("./Animations");
+		timer = this.GetNode<Timer>("./Timer");
+		errorLabel = this.GetNode<Label>("./ErrorLabel");
 
 		request = this.GetNode<HttpRequest>("HTTPRequest");
 
@@ -38,16 +43,34 @@ public partial class Login : Control
 		request.Request(apiUrl, headers, HttpClient.Method.Post, body);
 	}
 
-	private void _on_http_request_request_completed(long result, long responseCode, string[] headers, byte[] body){
+	private async void _on_http_request_request_completed(long result, long responseCode, string[] headers, byte[] body){
 		var json = new Json();
 		var parseResult = json.Parse(body.GetStringFromUtf8());
 		if (parseResult != Error.Ok)
 		{
 			GD.Print($"Api request error: {json.GetErrorMessage()} in {body.GetStringFromUtf8()} at line {json.GetErrorLine()}");
+			CredentialErrors();
 			return;
 		}
 
 		var apiData = new Godot.Collections.Dictionary<string, Variant>((Godot.Collections.Dictionary)json.Data);
-    	GD.Print(apiData);
+
+		if((String)apiData["name"] != username.Text || !(Boolean)apiData["password"] || !(Boolean)apiData["player"]){
+			GD.Print("Nada de nada");
+			CredentialErrors();
+		}else{
+			animation.Play("Login");
+			//TODO: Add selection panel
+			
+		}
+	}
+
+	private void CredentialErrors(){
+		errorLabel.Show();
+		timer.Start();
+	}
+
+	private void _on_timer_timeout(){
+		errorLabel.Hide();
 	}
 }
